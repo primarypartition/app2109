e0e09f03c5e17fbdc0b42393008d61454f3f1a9e
Your Name <you@example.com>
2021-01-31 15:03:58 -0700

    js

---
+++ js/js1/basicsCheatsheet.js
@@ -0,0 +1,54 @@
+// Numbers:
+1; -
+99;
+0.345345;
+NaN;
+
+typeof NaN
+typeof 4
+
+2 + 4;
+2 - 4;
+2 * 4;
+2 / 4;
+2 % 4;
+(2 + 4) + 6;
+3 ** 2
+
+
+//Making variables with let:
+let numberOfFriends = 1;
+var val = 10;
+
+clear();
+
+
+//Incrementing:
+numberOfFriends += 3; //numberOfFriends is now 4
+
+val -= val;
+val += val;
+val /= val;
+val %= val;
+
+val--
+val++
+
+
+// Variables with const
+const minimumAge = 21; //CANNOT REASSIGN!
+
+var num = 10;
+
+
+//Booleans - true or false values
+true;
+false;
+let isHappy = true;
+
+
+//Naming Conventions
+// Use upper camel-cased names:
+let numberOfChickens = 6; //GOOD
+// NOT THE JS WAY:
+// let number_of_chickens = 6;
